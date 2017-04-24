257978cccb06dace54a59eec17226117e1211f7e
Brian Bycynski <high5bri@hotmail.com>
2017-04-24 13:35:07 -0400

    started new portfolio

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
-
+import '../styles/grid.scss'
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
+    return <div className='wrapper'>
+      <div className='box header'>
+        Head
+      </div>
+      <div className='box sidebarL'>
+        Sidebar-left
+      </div>
+      <div className='box content'>
+        Content
+      </div>
+      <div className='sidebarR'>
+        Sidebar-right
+      </div>
+      <div className='box footer'>
+        Footer
+      </div>
     </div>
   }
 }
