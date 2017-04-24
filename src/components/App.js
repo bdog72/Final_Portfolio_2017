d77fa11a8f827e85fa38ee284c5d7ab5aa09c48f
Brian Bycynski <high5bri@hotmail.com>
2017-04-24 14:10:31 -0400

    started new portfolio

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react'
+import { Image } from 'react-bootstrap'
 import '../styles/grid.scss'
-class App extends Component {
+
+export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: '',
+      location: '',
+      imageURL: '',
+      bio: ''
+    }
+  }
+
+  componentDidMount () {
+    window.fetch('https://api.github.com/users/bdog72').then((response) => {
+      return response.json()
+    }).then((data) => {
+      this.setState({name: data.name, location: data.location, imageURL: data.avatar_url, bio: data.bio})
+    })
+  }
+
   render () {
     return <div className='wrapper'>
       <div className='box header'>
-        Head
+        <h1>{this.state.name}</h1>
+        <Image src={this.state.imageURL} responsive circle />
+        <p>{this.state.bio}</p>
+        <p>I'm a Jr. Front End Engineer in {this.state.location}</p>
       </div>
       <div className='box sidebarL'>
         Sidebar-left
       </div>
       <div className='box content'>
-        Content
+        <a href='https://twitter.com/Bdoggy72'>
+          <Image src='https://cdn1.iconfinder.com/data/icons/social-media-icon-1/112/twitter-128.png' responsive />
+        </a>
+        <p>My Current GitHub Chart</p>
+        <a href='https://github.com/bdog72'>
+          <Image src='http://ghchart.rshah.org/bdog72' alt='Brians GitHub chart' responsive />
+        </a>
       </div>
       <div className='sidebarR'>
         Sidebar-right
       </div>
       <div className='box footer'>
-        Footer
+        <p>813-505-4733</p>
+        <p>high5bri&#64;hotmail.com</p>
+        <p>Made With &hearts; From What I Learned At The Iron Yard</p>
       </div>
     </div>
   }
 }
-
-export default App
